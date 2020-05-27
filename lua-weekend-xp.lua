fafeb62a0c42d7a81528783a032bcc1018bf39b2
iThorgrim <ithogrim@outlook.com>
2020-05-27 12:12:32 +0200

    Update lua-weekend-xp.lua

--- lua-weekend-xp.lua
+++ lua-weekend-xp.lua
@@ -1,16 +1,16 @@
 --@Author: iThorgrimHub#1775
 --@Date:   2019-11-13 22:49
 
-local Days = {'Saturday', 'Sunday'};
+local Days = {"Saturday", "Sunday"}
 
-local multi = 2; -- 2 == exp x2 (Basic rate x multi)
+local multi = 2 -- 2 == exp x2 (Basic rate x multi)
 
 local function getExp(event, player, amount, _)
-  for _, day in ipairs(Days)do
-    local getDate = os.date("%A");
-    if (getDate == day)then
-      return amount * multi;
+    for _, day in ipairs(Days) do
+        local getDate = os.date("%A")
+        if (getDate == day) then
+            return amount * multi
+        end
     end
-  end
 end
-RegisterPlayerEvent(12, getExp);
+RegisterPlayerEvent(12, getExp)
